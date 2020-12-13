009d0e4b654a23962e3b5ef161bc22f6a2e2a50e
Sviat98 <bashkevich.98@gmail.com>
2020-12-13 17:23:40 +0300

    added loading movies via coroutines from JSON file

--- app/src/main/java/com/bashkevich/androidfundamentals/model/Actor.kt
+++
@@ -1,9 +0,0 @@
-package com.bashkevich.androidfundamentals.model
-
-import androidx.annotation.DrawableRes
-
-data class Actor(
-    @DrawableRes
-    val imageResource: Int,
-    val name: String
-)
